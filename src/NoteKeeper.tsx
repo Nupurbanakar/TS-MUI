b379f9b1b65314540e55a75e537ee59b9ecd0b45
Nupur Banakar <nupurbanakar@gmail.com>
2024-05-07 12:09:38 +0530

    Added Modal Textfield and save button

--- src/NoteKeeper.tsx
+++ src/NoteKeeper.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import {Fab} from '@mui/material';
+import { Fab, Modal, Box, TextField, Button } from '@mui/material';
 import addNote from './assets/plus.png'
+import plus from './assets/plus-icon.png'
 import '../src/index.css'
 
 const NoteKeeper: React.FC = () => {
@@ -31,6 +32,38 @@ const NoteKeeper: React.FC = () => {
       <Fab color="primary" aria-label="add" onClick={handleOpen} >
         <img src={addNote} alt='add-note' className='img'></img>
       </Fab>
+      <Modal open={open} onClose={handleClose}>
+        <Box
+          sx={{
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            bgcolor: '#ffffff',
+            borderRadius: '10px',
+            boxShadow: 24,
+            p: 4,
+            maxWidth: 600,
+            width: '100%',
+          }}
+        >
+          <TextField
+            id="note"
+            label="Enter your note"
+            multiline
+            minRows={3}
+            maxRows={10}
+            value={note}
+            onChange={handleNoteChange}
+            fullWidth
+            className='textbox'
+          />
+          <Button variant="contained" onClick={handleSaveNote} className='save-btn'>
+          <img src={plus} alt='add-note' className='plus'></img>
+          <span style={{ flex: 1 }} ><strong>Save</strong></span>
+          </Button>
+        </Box>
+      </Modal>
     </div>
     </>
   );
